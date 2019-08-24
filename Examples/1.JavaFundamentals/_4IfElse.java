f57dd281d02d442f6f50e637a683ed1c11b0c404
ae <erradi@live.com>
2019-08-24 22:18:01 +0300

    Updates Java Fundamentals slides and examples

--- Examples/1.JavaFundamentals/_4IfElse.java
+++ Examples/1.JavaFundamentals/_4IfElse.java
@@ -1,47 +1,15 @@
 import java.util.Scanner;
 
-public class _4IfElse
-{
-    public static void main(String[] args)
-    {
-        // welcome the user to the program
-        System.out.println("Welcome to the Invoice Total Calculator");
-        System.out.println();  // print a blank line
+public class _4IfElse {
+	public static void main(String arg[]) {
+		Scanner input = new Scanner(System.in);
+		int x;
+		System.out.print("Enter an integer: ");
+		x = input.nextInt();
 
-        // create a Scanner object named sc
-        Scanner scanner = new Scanner(System.in);
-
-        // perform invoice calculations until choice isn't equal to "y" or "Y"
-        String choice = "y";
-        while (choice.equalsIgnoreCase("y"))
-        {
-            // get the invoice subtotal from the user
-            System.out.print("Enter invoice amount:   ");
-            double invoiceAmount = scanner.nextDouble();
-
-            // calculate the discount amount and total
-            double discountPercent;
-            if (invoiceAmount >= 200)
-                discountPercent = .2;
-            else if (invoiceAmount >= 100)
-                discountPercent = .1;
-            else
-                discountPercent = 0.0;
-            
-            double discountAmount = invoiceAmount * discountPercent;
-            double amountToPay = invoiceAmount - discountAmount;
-
-            // display the discount amount and total
-            String message = "Discount percent: " + discountPercent + "\n"
-                    + "Discount amount:  " + discountAmount + "\n"
-                    + "Invoice total:    " + amountToPay + "\n";
-            System.out.println(message);
-
-            // see if the user wants to continue
-            System.out.print("Continue? (y/n): ");
-            choice = scanner.next();
-            System.out.println();
-        }
-        scanner.close();
-    }
+		if (x % 2 == 0)
+			System.out.printf("%d %s", x, "is even number");
+		else
+			System.out.printf("%d %s", x, "is odd number");
+	}
 }
