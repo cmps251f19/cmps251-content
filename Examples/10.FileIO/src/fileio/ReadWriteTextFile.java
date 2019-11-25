75b8862f76de8c8bbd077f7ed135f72379c2f803
ae <erradi@live.com>
2019-11-25 16:06:51 +0300

    Minor fix to JavaIO examples

--- Examples/10.FileIO/src/fileio/ReadWriteTextFile.java
+++ Examples/10.FileIO/src/fileio/ReadWriteTextFile.java
@@ -2,59 +2,53 @@ package fileio;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.APPEND;
 
 public class ReadWriteTextFile {
 
 	public static void main(String[] args) {
-		//List all files
-		String path = "data";
+		String inputFileName = "data/countries.txt";
+		String outputFileName = "data/me-countries.txt";
+		List<String> countries;
 		try {
-			List<String> files = Files.list(Paths.get(path))
-									  .filter(p -> p.toString().endsWith(".json"))
-									  .map(p -> p.toString())
-									  .collect(Collectors.toList());
+			countries = Files.lines(Paths.get(inputFileName))
+							 .filter(c -> c.contains("Middle East"))
+							 .map(c -> c.split(";")[0])
+							 .sorted()
+							 .collect(Collectors.toList());
+
+			System.out.println(countries);
 			
-			System.out.printf("%nThere are %d .json files in %s%n", files.size(), path);
+			Files.write(Paths.get(outputFileName), countries);
 			
-			files.forEach(System.out::println);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		//List all folders
-		path = "D:\\cmps251\\cmps251-content";
-		try {
-			List<String> folders = Files.list(Paths.get(path))
-										.filter(Files::isDirectory)
-										.map(p -> p.toString())
-										.collect(Collectors.toList());
+			//In case we want to append to the file instead of overwrite we can pass these option
+			OpenOption[] options = new OpenOption[] { CREATE, APPEND };
 			
-			System.out.printf("%nThere are %d folders in %s%n", folders.size(), path);
-			folders.forEach(System.out::println);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println(".java Files in cmps251-content folder:");
-		path = "D:\\cmps251\\cmps251-content";
-		try {
-			List<String> files = Files.walk(Paths.get(path))
-			 	 .filter(p -> p.getFileName().toString().endsWith(".java"))
-			 	 .map(p -> p.toString())
-			 	 .collect(Collectors.toList());
+			List<String> lines =
+					List.of("Line One", "Line Two", "Final Line");
 			
-			System.out.printf("%nThere are %d .java files in %s%n", files.size(), path);
-			files.forEach(System.out::println);
+			Files.write(Paths.get(outputFileName), lines, options); 
+			
+			System.out.printf("%nThere are %d characters in %s", 
+						countFileLetters(inputFileName),
+						inputFileName);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private static long countFileLetters(String inputFileName) throws IOException {
+		return Files.lines(Paths.get(inputFileName))
+					.mapToInt(String::length)
+					.sum();
 	}
 }
